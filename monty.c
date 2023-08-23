4625f130ec4e5b077c4bda6f415882b55c512efb
muriukialex-n <muriukialex.n@gmail.com>
2023-08-23 14:35:10 -0700

    done tasks

---
+++ monty.c
@@ -0,0 +1,24 @@
+#include "monty.h"
+global_var var_global;
+/**
+ * main - driver function of the monty program
+ * @ac: int number of the arguments
+ * @av: the opcode file
+ * Return: 0
+ */
+int main(int ac, char **av)
+{
+	stack_t *stack;
+
+	stack = NULL;
+	if (ac != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+
+	read_file(av[1], &stack);
+    /* recordar liberar memorias */
+	free_dlistint(stack);
+	return (0);
+}
