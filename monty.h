72574e86372f4f8928f4ee20f569bea0bc3f3803
muriukialex-n <muriukialex.n@gmail.com>
2023-08-23 16:37:43 -0700

    done

--- monty.h
+++ monty.h
@@ -78,6 +78,8 @@ void pchar(stack_t **stack, unsigned int line_number);
 void rotl(stack_t **stack, unsigned int line_number);
 void pchar(stack_t **stack, unsigned int line_number);
 void rotr(stack_t **stack, unsigned int line_number);
+void f_push(stack_t **stack, unsigned int line_number);
+void f_pall(stack_t **stack, unsigned int line_number);
 
 
 #endif /* MONTY_H */
